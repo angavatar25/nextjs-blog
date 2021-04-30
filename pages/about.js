e6b5703e4c26ee62bb244c66b693df8bbc71f20a
Ananda Afryan Gemilang <anandalark007@gmail.com>
2021-04-30 11:00:24 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Layout.module.css';
+
+const about = () => {
+    return (
+        <div className={styles.container}>
+            <h1>About Page</h1>
+        </div>
+    )
+}
+
+export default about;
---
+++ pages/article/[id]/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {useRouter} from 'next/router'
+import Link from 'next/link';
+
+const article = ({article}) => {
+    const router = useRouter()
+    const {id} = router.query
+    console.log(article.title)
+    return (
+        <div>
+            This is an article {article.id}
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <br/>
+            <Link href="/">Go back</Link>
+        </div>
+    )
+}
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+    const articles = await res.json()
+    const ids = articles.map((article) => article.id)
+    const paths = ids.map((id) => ({params: {id: id.toString()}}))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export default article
